0ea6b5566285f8c489abddedf25f88af611fd6bf
Bhavika Shrivastava <shrivastava.bhavika@gmail.com>
2022-05-24 08:56:46 +0530

    add operation

---
+++ add.py
@@ -0,0 +1,8 @@
+a=10
+b=35
+def add(x,y):
+    return x+y;
+c=add(a,b)
+print("sum is "+str(c))
+print("sum is printed.")
+print("the end")
---
+++ multiply.py
@@ -0,0 +1,8 @@
+a=10
+b=35
+def multiply(x,y):
+    return x*y;
+c=multiply(a,b)
+print("product is "+str(c))
+print("product is printed.")
+print("the end")
